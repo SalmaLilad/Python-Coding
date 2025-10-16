439c75a9abab401b6f8ba138c13dcd03de86cac1
SalmaLilad <140752716+SalmaLilad@users.noreply.github.com>
2025-10-15 19:38:12 -0500

    Remove rotate_img_coords function
    Removed the rotate_img_coords function to simplify image processing.

--- Sunglasses_on_Eyes.py
+++ Sunglasses_on_Eyes.py
@@ -151,30 +151,6 @@ def display_angled_sunglasss(face_number, anchor):
     else:
         return base_plus_lens(base_img,sunglasses_img,  dest = right_eye_xy, angle = angle_eyes, scale = estimated_scale)
 
-  import math
-def rotate_img_coords(x,y,w,h,theta_radians = 0):
-    
-    r = math.sqrt(x**2 + y**2) # replace so that it matches the expression for r.
-    gamma_radians = math.atan2(y,x)
-    
-    ## Assume theta is counter clockwise rotation
-    net_rotation = gamma_radians - theta_radians
-    
-    x_bar = r * math.cos(net_rotation) 
-    y_bar = r * math.sin(net_rotation)
-    
-    if theta_radians <= 0 :
-        x_new = x_bar + h * math.sin(theta_radians)
-        y_new = y_bar
-    else:
-        x_new = x_bar
-        y_new = y_bar + w * math.sin(theta_radians)
-    
-    x_new = int(x_new) # make them integers since x, y coordinates of image are integers
-    y_new = int(y_new)
-    
-    return x_new, y_new 
-
 import math 
 def compute_rotated_lens_dest(img_left_eye_xy,sun_left_eye_xy,sun_width, sun_height,angle_eyes_degrees,new_scale):
     
